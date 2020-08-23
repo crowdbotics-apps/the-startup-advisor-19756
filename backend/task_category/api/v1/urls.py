598bd4978cd2c970bf996c240f10133bc56f69dc
Crowdbotics <team@crowdbotics.com>
2020-08-23 21:13:08 +0000

    Update models

--- backend/task_category/api/v1/urls.py
+++ backend/task_category/api/v1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import SubcategoryViewSet, CategoryViewSet
 
 router = DefaultRouter()
+router.register("category", CategoryViewSet)
+router.register("subcategory", SubcategoryViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
